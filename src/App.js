9b5f96bcb7bc1cf0d62e5e1002c4ba82be79c81d
“GalleryGirl” <audreysmitchell@gmail.com>
2020-03-22 23:45:24 -0500

    Add back head and body tags

--- src/App.js
+++ src/App.js
@@ -30,18 +30,23 @@ import './App.css';
 function App() {
   return (
 
-    
-  
-    <div class="clock">
-      <div class="clock-face">
-        <div class="hand hour-hand"></div>
-        <div class="hand min-hand"></div>
-        <div class="hand second-hand"></div>
-      </div>
-    </div>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8"/>
+        <title>JS + CSS Clock</title>
+      </head>
+      <body>
+      
+        <div class="clock">
+          <div class="clock-face">
+            <div class="hand hour-hand"></div>
+            <div class="hand min-hand"></div>
+            <div class="hand second-hand"></div>
+          </div>
+        </div>
 
-  
-
+      </body>
+    </html>
   );
 }
 
